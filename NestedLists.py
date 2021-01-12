a9dd20517b8bb3b2ac4f6a542b939a1fe5f97459
Laasya Setty <likithalaasya@gmail.com>
2021-01-12 15:47:15 +0530

    recheck
    incomplete-changes requires

---
+++ NestedLists.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    arr=[]
+    for _ in range(int(input())):
+        arr.append([])
+        name = input()
+        score = float(input())
+        arr[_].append(name)
+        arr[_].append(score)
+#    for i in range(len(arr)):
