097538d18c20ab2501ebe2241f9a3628c47a56d2
Laasya Setty <likithalaasya@gmail.com>
2021-01-14 12:40:42 +0530

    initial
    completed

---
+++ decorator.py
@@ -0,0 +1,20 @@
+def smart_divide(func):
+    def inner(a,b):
+        print("Going to divide a and b")
+        if(b==0):
+           print("Sorry! You are not divisible")
+           return
+        return func(a,b)
+    return inner
+
+@smart_divide
+def divide(a,b):
+    print(a/b)
+divide(2,0)
+
+# lines 10 to 13 are equivalent too
+""""def divide(a,b):
+    print(a/b)
+    divide=smart_divide(divide)"""""
+    
+        
